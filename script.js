db9aa18fb932f8ce1d83efedbda640c6a7b56815
Selucus <bchapman141@gmail.com>
2024-08-22 23:04:48 +0100

    'changes'

---
+++ script.js
@@ -0,0 +1,66 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+
+function addGoal(){
+    if(inputBox.value === ''){
+        alert("enter your goal");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.prepend(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function sortList(){
+    const frag = document.createDocumentFragment();
+    const items = listContainer.querySelectorAll("li");
+    for (let item of items){
+        if(!item.classList.contains("checked")){
+            frag.appendChild(item);
+        }
+    }
+    for (let item of items){
+        if(item.classList.contains("checked")){
+            frag.appendChild(item);
+        }
+    }
+    
+    listContainer.appendChild(frag);
+}
+
+function showList(){
+    
+    listContainer.innerHTML = localStorage.getItem("data");
+    
+    sortList();
+
+    
+}
+
+showList();
